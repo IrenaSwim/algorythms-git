2d4a4d90fbc1574eff8624c6cf51d83bc8526a86
Irena <i.kilyakova89@gmail.com>
2026-04-25 23:15:13 +0500

    added theme 5 and files with funcs and stack chart for fib5

---
+++ theme5/funcs.py
@@ -0,0 +1,106 @@
+import random
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+        
+        merge_sort(left)
+        merge_sort(right)
+        
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+        
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+    return arr
+    
+list20 = [random.randint(1,1000) for _ in range(20)]
+print(list20)
+#print()
+#print(merge_sort(list10)) 
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[len(arr) // 2]
+        left = [] 
+        right = []  
+        mid = []
+        for item in arr:
+            if item < pivot:
+                left.append(item)  
+            elif item > pivot:
+                right.append(item)   
+            else:
+                mid.append(item) 
+                
+        return quick_sort(left) + mid + quick_sort(right)   
+        
+#print(quick_sort(list20)) 
+
+def max_el(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+        
+        merge_sort(left)
+        merge_sort(right)
+        
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                k = right[j]
+                i += 1
+            else:
+                k = left[i]
+                j += 1
+        
+        while i < len(left):
+            k = left[i]
+            i += 1
+        
+        while j < len(right):
+            k = right[j]
+            j += 1
+    return k
+    
+#print(max_el(list20))  
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)     
+
+#print(fibonacci(5))
+
+def insertion_sort(arr):
+    for ind in range(1, len(arr)):
+        key = arr[ind]
+        j = ind - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr    
+            
+print(insertion_sort(list20))           
+    
