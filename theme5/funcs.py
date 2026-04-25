82d25c1e8bbb927b63bc2c30e436498d4c98334a
Irena <i.kilyakova89@gmail.com>
2026-04-25 23:44:44 +0500

    corrected max el func

--- theme5/funcs.py
+++ theme5/funcs.py
@@ -1,24 +1,32 @@
 import random
 
-def merge_sort(arr):
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)     
+
+print(fibonacci(5))
+print()
+
+def max_el(arr):
     if len(arr) > 1:
         mid = len(arr) // 2
         left = arr[:mid]
         right = arr[mid:]
         
-        merge_sort(left)
-        merge_sort(right)
+        max_el(left)
+        max_el(right)
         
         i = j = k = 0
         while i < len(left) and j < len(right):
             if left[i] < right[j]:
-                arr[k] = left[i]
+                arr[k] = right[j]
                 i += 1
             else:
-                arr[k] = right[j]
+                arr[k] = left[i]
                 j += 1
             k += 1
-        
         while i < len(left):
             arr[k] = left[i]
             i += 1
@@ -27,13 +35,15 @@ def merge_sort(arr):
         while j < len(right):
             arr[k] = right[j]
             j += 1
-            k += 1
-    return arr
-    
+            k += 1       
+
+    return arr[-1]
+
 list20 = [random.randint(1,1000) for _ in range(20)]
 print(list20)
-#print()
-#print(merge_sort(list10)) 
+print()    
+print(f'Наибольшее число в списке: {max_el(list20)}')
+print()
 
 def quick_sort(arr):
     if len(arr) <= 1:
@@ -52,45 +62,11 @@ def quick_sort(arr):
                 mid.append(item) 
                 
         return quick_sort(left) + mid + quick_sort(right)   
-        
-#print(quick_sort(list20)) 
 
-def max_el(arr):
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left = arr[:mid]
-        right = arr[mid:]
-        
-        merge_sort(left)
-        merge_sort(right)
-        
-        i = j = k = 0
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                k = right[j]
-                i += 1
-            else:
-                k = left[i]
-                j += 1
-        
-        while i < len(left):
-            k = left[i]
-            i += 1
-        
-        while j < len(right):
-            k = right[j]
-            j += 1
-    return k
-    
-#print(max_el(list20))  
-
-def fibonacci(n):
-    if n <= 1:
-        return n
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)     
-
-#print(fibonacci(5))
+list10 = [random.randint(1,100) for _ in range(10)]
+print(list10)
+print()        
+print(quick_sort(list10)) 
 
 def insertion_sort(arr):
     for ind in range(1, len(arr)):
@@ -102,5 +78,5 @@ def insertion_sort(arr):
         arr[j + 1] = key
     return arr    
             
-print(insertion_sort(list20))           
+#print(insertion_sort(list20))           
     
