c3bdd8b7a7e75f356f1949494245e92694ca0e5d
Irena <i.kilyakova89@gmail.com>
2026-05-09 00:53:12 +0500

    add new funcs in adj_matrix

--- theme9/adj_matrix_list.py
+++ theme9/adj_matrix_list.py
@@ -1,35 +1,73 @@
 import numpy as np
+from collections import defaultdict
 
-def adj_matrix(v_num, edges):
-    matrix = np.zeros((v_num, v_num))
+def adj_matrix_graph(vertex_num, edges, indirected=False):
+    matrix = [[0] * vertex_num for _ in range(vertex_num)]
     
-    for edge in edges:
-        u,v = edge
+    for u, v in edges:
         matrix[u][v] = 1
-    for row in matrix:
-        print(row)   
-    return matrix 
+        if indirected:
+            matrix[u][v] = 1
+            
+    return matrix    
     
-eds = [(0, 3), (2, 2), (3, 1)]  
-
-mgraph = adj_matrix(4, eds)
+       
+def insert_vertex(graph):
+    graph.append([0]*len(graph))
+    for row in graph:
+        row.append(0)
+    return graph
     
-def add_edge(graph, edge):
-    u,v = edge
+def insert_edage(graph, edge, indirected=False):
+    u, v = edge
     graph[u][v] = 1
-    return graph
+    if indirected:
+        graph[v][u] = 1
+    
+    return graph  
+        
 
-def add_vertex(graph): 
-    colomn = np.zeros(len(graph))
-    graph = np.insert(graph, len(graph), colomn, axis=1)
-    row = np.zeros((len(graph) + 1))
-    graph = np.insert(graph, len(graph), row, axis=0)
-    return graph
-
-add_edge(mgraph, (3,0))   
-print() 
-print(mgraph)
-print()
-mgraph = add_vertex(mgraph)
-for row in mgraph:
+edges2 = [(0, 2), (2, 3), (3, 1)]
+m_graph = adj_matrix_graph(4, edges2)  
+insert_vertex(m_graph)
+for row in m_graph:
     print(row)
+print()    
+insert_edage(m_graph, (4, 1))  
+for row in m_graph:
+    print(row)
+
+# вариант с библиотекой numpy
+# def adj_matrix(v_num, edges):
+#     matrix = np.zeros((v_num, v_num))
+    
+#     for edge in edges:
+#         u,v = edge
+#         matrix[u][v] = 1
+#     for row in matrix:
+#         print(row)   
+#     return matrix 
+    
+# eds = [(0, 3), (2, 2), (3, 1)]  
+
+# mgraph = adj_matrix(4, eds)
+    
+# def add_edge(graph, edge):
+#     u,v = edge
+#     graph[u][v] = 1
+#     return graph
+
+# def add_vertex(graph): 
+#     colomn = np.zeros(len(graph))
+#     graph = np.insert(graph, len(graph), colomn, axis=1)
+#     row = np.zeros((len(graph) + 1))
+#     graph = np.insert(graph, len(graph), row, axis=0)
+#     return graph
+
+# add_edge(mgraph, (3,0))   
+# print() 
+# print(mgraph)
+# print()
+# mgraph = add_vertex(mgraph)
+# for row in mgraph:
+#     print(row)
